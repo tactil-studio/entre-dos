1e4016aeaaab7cb25a97eee5cf702213d958e19d
Francesco Fabrissin <francesco.fabrissin@meona.de>
2026-04-22 12:51:28 +0200

    Removed FloralBackdrop component from Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,31 +1,29 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
-import MenuPreviewSection from "@/components/MenuPreviewSection";
-import GallerySection from "@/components/GallerySection";
-import ReviewsSection from "@/components/ReviewsSection";
-import LocationSection from "@/components/LocationSection";
-import SpotifySection from "@/components/SpotifySection";
 import FooterSection from "@/components/FooterSection";
-import FloralBackdrop from "@/components/FloralBackdrop";
+import GallerySection from "@/components/GallerySection";
+import HeroSection from "@/components/HeroSection";
+import LocationSection from "@/components/LocationSection";
+import MenuPreviewSection from "@/components/MenuPreviewSection";
+import Navbar from "@/components/Navbar";
+import ReviewsSection from "@/components/ReviewsSection";
+import SpotifySection from "@/components/SpotifySection";
 
 const Index = () => {
-  return (
-    <div className="min-h-screen relative isolation-isolate">
-      <FloralBackdrop />
-      <div className="relative z-10">
-        <Navbar />
-        <HeroSection />
-        <AboutSection />
-        <MenuPreviewSection />
-        <GallerySection />
-        <ReviewsSection />
-        <LocationSection />
-        <SpotifySection />
-        <FooterSection />
-      </div>
-    </div>
-  );
+	return (
+		<div className="min-h-screen relative isolation-isolate">
+			<div className="relative z-10">
+				<Navbar />
+				<HeroSection />
+				<AboutSection />
+				<MenuPreviewSection />
+				<GallerySection />
+				<ReviewsSection />
+				<LocationSection />
+				<SpotifySection />
+				<FooterSection />
+			</div>
+		</div>
+	);
 };
 
 export default Index;
